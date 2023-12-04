5f3a60d190d0eb6898c56cac7d8667f1677e574a
goldirana <goldirana3210@gmail.com>
2023-12-04 23:10:20 +0000

    mlops_first_chapter_learning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will be used to learn mlops',
+    author='goldy',
+    license='MIT',
+)
